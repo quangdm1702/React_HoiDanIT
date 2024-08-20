71a85b5b7308e336aafc662fb8c4738547d603ed
quangdm1702 <doanminhquang17022003@gmail.com>
2024-08-21 05:15:53 +0700

    Video 22

--- src/components/MyComponent.js
+++ src/components/MyComponent.js
@@ -1,49 +1,14 @@
 import React from "react";
+import UserInfor from "./UserInfor";
 
 class MyComponent extends React.Component { // ke thua lai tinh nang component cua react
     //JSX: cho phep viet code JS trong HTML
 
-    state = {
-        name: 'Quang',
-        age: 21,
-        address: 'Ha Noi'
-    };
-
-    handleClick(event) {
-        console.log('>>> Click me');
-        // merger state chi trong react class
-        this.setState({
-            name: 'Minh Quang',
-            age: Math.floor((Math.random() * 100) + 1)
-        })
-    }
-
-    handleOnMoverOver(event) {
-        // console.log(event.pageX);
-    }
-
-    handleOnChangeInput = (event) => {
-        this.setState({
-            name: event.target.value,
-        })
-    }
-
-    handleOnSubmit = (event) => {
-        event.preventDefault();
-        console.log(this.state);
-    }
 
     render() {
         return (
             <div>
-                My name is {this.state.name} and I'm  {this.state.age}
-                <form onSubmit={(event) => this.handleOnSubmit(event)}>
-                    <input
-                        type="text"
-                        onChange={(event) => this.handleOnChangeInput(event)}
-                    />
-                    <button>Submit</button>
-                </form>
+                <UserInfor />
             </div>
         );
     }
